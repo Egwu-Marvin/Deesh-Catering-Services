f071a8732e13df88d9242a1d902ac6013f143d30
Egwu-Marvin <egwumarvin@gmail.com>
2024-07-06 13:57:07 +0000

    add:

--- src/app/components/Audio.jsx
+++ src/app/components/Audio.jsx
@@ -3,13 +3,7 @@ import React from 'react'
 export default function Audio() {
   return (
     <div>
-      <span className=''>
-        <audio autoPlay loop alt='food audio' width={100} height={20} >
-        <source src="/audio/deesh-front4.MP3"  type='audio/mp3' />
-        <source src="/audio/deesh-frontAd.MP3"  type='audio/mp3' />
-        <source src="/audio/deesh-frontAdw.WAV"  type='audio/wav' />
-        </audio>
-        </span>
+      
     </div>
   )
 }
