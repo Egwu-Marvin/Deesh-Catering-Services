0a7f01c419e0233976d8449b6a13293784c708d0
Egwu-Marvin <egwumarvin@gmail.com>
2024-07-06 14:18:04 +0000

    updating

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,7 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Audio from "./components/audio";
+// import Audio from "./components/audio";
 
 const inter = Inter({ subsets: ["latin"] });
 
