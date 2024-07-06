ec7e02bed32c3be195ce38dccc54a3ae76649eed
Egwu-Marvin <egwumarvin@gmail.com>
2024-07-06 14:11:15 +0000

    updating

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Audio/>
+        {/* <Audio/> */}
         <Navbar/>
 
         <div className="min-h-screen text-4xl">
